d5e019503044c97280be47c221a9642b01353cbd
nauxscript <nauxscript@gmail.com>
2023-09-12 10:22:02 +0800

    chore: setup basic dev config

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         tampermonkey-starter
+// @license      MIT
+// @version      0.0.1
+// @description  油猴脚本起步模板
+// @author       Nauxscript
+// @run-at       document-start
+// @match        */swagger-ui/*
+// @match        */*/swagger-ui/*
+// @match        */*/*/swagger-ui/*
+// @namespace    Nauxscript
+// @require      https://cdn.bootcdn.net/ajax/libs/jquery/3.6.4/jquery.js
+// ==/UserScript==
+
+(function () {
+  'use strict'
+})()
